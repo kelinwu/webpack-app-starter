ceb17837c458ada3639185535bc6edb3e499b7fc
Kelin Wu <kelin.wu@sas.com>
2017-04-20 10:14:12 -0400

    init commit

---
+++ source/js/people.js
@@ -0,0 +1,12 @@
+// function gethello() {
+//     return 'Hello there';
+// }
+
+let people =[
+    {name: 'John Doe'},
+    {name: 'John Two'},
+    {name: 'John Three'}
+
+]
+
+module.exports = people;
